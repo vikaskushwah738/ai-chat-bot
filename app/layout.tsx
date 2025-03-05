ea4fb06b903e2cd0ea6271356d7f8a749d61224e
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-03-05 17:31:57 +0530

    Can't resolve '@/components/footer' error reloved

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
