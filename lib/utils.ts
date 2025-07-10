3bfd61f80a47cefc60ee6865d4edaa72d4339548
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-07-10 15:55:41 +0530

    minner upadtes

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const generateId = (): string => Math.random().toString(36).slice(2, 15);
